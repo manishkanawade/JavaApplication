4dfa21f1defe17d08a8eb796640832caaa682df5
manishkanawade <manishkanawade007@gmail.com>
2023-06-15 11:01:39 +0530

    thursday

--- src/com/tcs/Test/Test.java
+++ src/com/tcs/Test/Test.java
@@ -4,19 +4,9 @@ public class Test {
 
 	public static void main(String[] args) {
 		
-		System.out.println("master branch code");
+	System.out.println("eclipse java");
 		
-		System.out.println("developer branch code");
 		
-		System.out.println("changes done in eclipse java workspace");
-		
-		System.out.println("changes done in Expandables");
-		
-		System.out.println("code added ");
-		
-		System.out.println("code added 1");
-		
-		System.out.println("thursday");
 
 	}
 
